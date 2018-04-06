aa7fc10feae7d8cc6e75171e1527d5e820d78f96
admin <admin@admin-PC>
2018-04-06 15:26:34 +0530

    controller

---
+++ BankingManagement/src/main/java/com/mockathon/BankingManagement/Controller/BankingManagementController.java
@@ -0,0 +1,9 @@
+package com.mockathon.BankingManagement.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BankingManagementController {
+
+	
+}
