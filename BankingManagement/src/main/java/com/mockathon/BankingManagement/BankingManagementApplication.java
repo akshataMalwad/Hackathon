7bc4225989e208533d7c4c84cd45118fa11b35d5
akshata <akshata.malwad@gmail.com>
2018-04-06 15:24:08 +0530

    base code

---
+++ BankingManagement/src/main/java/com/mockathon/BankingManagement/BankingManagementApplication.java
@@ -0,0 +1,12 @@
+package com.mockathon.BankingManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingManagementApplication.class, args);
+	}
+}
---
+++ BankingManagement/src/test/java/com/mockathon/BankingManagement/BankingManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mockathon.BankingManagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankingManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
